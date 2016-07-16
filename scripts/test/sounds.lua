3d50626341ef5f9fedd3eba3b4d7a7842e44e98e
SDraw <SDraw@users.noreply.github.com>
2016-07-17 01:26:32 +0300

    Update sounds.lua

--- scripts/test/sounds.lua
+++ scripts/test/sounds.lua
@@ -1,6 +1,3 @@
 data.sound = {}
 
 data.sound["background"] = soundCreate("sound/bgm01.ogg",true)
-soundSet3DPositionEnabled(data.sound["background"],true)
-soundSet3DPosition(data.sound["background"], 0.0,0.0,0.0)
-soundSet3DDistance(data.sound["background"], 20.0,50.0)
