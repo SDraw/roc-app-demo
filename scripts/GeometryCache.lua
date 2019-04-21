5cd0a6d204f2f74b6efe59e32e9d12eb109c6044
SDraw <sdraw5130@gmail.com>
2019-04-21 23:18:31 +0400

    Update to new asynchronous loading

--- scripts/GeometryCache.lua
+++ scripts/GeometryCache.lua
@@ -5,37 +5,52 @@ addEvent("onGeometryCacheLoad")
 function GeometryCache.init()
     local self = GeometryCache
     
-    self.ms_cache = {
-        skybox = Geometry("models/skybox.rmf",true),
-        plane = Geometry("models/plane.rmf",true),
-        cube = Geometry("models/cube.rmf",true),
-        cone = Geometry("models/cone.rmf",true),
-        cylinder = Geometry("models/cylinder.rmf",true),
-        sphere = Geometry("models/icosphere.rmf",true),
-        dummy = Geometry("models/dummy.rmf",true)
+    self.ms_tasks = {
+        [Geometry("models/skybox.rmf",self.onGeometryLoad)] = {
+            name = "skybox"
+        },
+        [Geometry("models/plane.rmf",self.onGeometryLoad)] = {
+            name = "plane"
+        },
+        [Geometry("models/cube.rmf",self.onGeometryLoad)] = {
+            name = "cube"
+        },
+        [Geometry("models/cone.rmf",self.onGeometryLoad)] = {
+            name = "cone"
+        },
+        [Geometry("models/cylinder.rmf",self.onGeometryLoad)] = {
+            name = "cylinder"
+        },
+        [Geometry("models/icosphere.rmf",self.onGeometryLoad)] = {
+            name = "sphere"
+        },
+        [Geometry("models/dummy.rmf",self.onGeometryLoad)] = {
+            name = "dummy"
+        }
     }
-    self.ms_cacheCount = 0
-    for k,v in pairs(self.ms_cache) do
-        v:setData("geometry:cache",k)
-        self.ms_cacheCount = self.ms_cacheCount+1
+    self.ms_tasksCount = 0
+    for k,v in pairs(self.ms_tasks) do
+        self.ms_tasksCount = self.ms_tasksCount+1
     end
-    self.ms_cacheResult = 0
+    self.ms_tasksResult = 0
     
-    addEventHandler("onGeometryLoad",self.onGeometryLoad)
+    self.ms_cache = {}
 end
 addEventHandler("onEngineStart",GeometryCache.init)
 
-function GeometryCache.onGeometryLoad(ud1,bool1)
+function GeometryCache.onGeometryLoad(ud1,ud2)
     local self = GeometryCache
     
-    if(ud1:getData("geometry:cache")) then
-        ud1:removeData("geometry:cache")
-        if(bool1) then
-            self.ms_cacheResult = self.ms_cacheResult+1
-            if(self.ms_cacheResult == self.ms_cacheCount) then
-                removeEventHandler("onGeometryLoad",self.onGeometryLoad)
+    if(self.ms_tasks[ud1]) then
+        if(ud2) then
+            self.ms_cache[self.ms_tasks[ud1].name] = ud2
+            
+            self.ms_tasksResult = self.ms_tasksResult+1
+            if(self.ms_tasksResult == self.ms_tasksCount) then
                 callEvent("onGeometryCacheLoad")
             end
+        else
+            logPrint("Unable to load geometry for '"..self.ms_tasks[ud1].name.."'")
         end
     end
 end
